9ff8de8fa31ab502ce7ba110afa6d35820ec90f6
matthewwalk <matt.walker98@outlook.com>
2021-06-07 14:54:27 -0400

    Algorithm Update

--- code/jpf-partial-transition-system-listener/src/test/partialtransitionsystemlistener/Graph.java
+++ code/jpf-partial-transition-system-listener/src/test/partialtransitionsystemlistener/Graph.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Graph {
-	
+
 	private static final long seed = System.currentTimeMillis();
 	private Map<Integer, List<Integer>> graph;
 
@@ -18,10 +18,15 @@ public class Graph {
 		// Generate empty graph structure
 		graph = new LinkedHashMap<Integer, List<Integer>>(nodes);
 		// Begin graph generation
-		IntStream.range(0, nodes).forEach(value -> {
-			graph.computeIfAbsent(value, k -> new ArrayList<Integer>()).addAll(
-					r.ints(0, nodes).distinct().limit(r.nextInt(maxEdges + 1)).boxed().collect(Collectors.toList()));
-		});
+		double probability = Math.log(nodes) / nodes;
+		for (int i = 0; i < nodes; i++) {
+			graph.put(i, new ArrayList<Integer>());
+			for (int j = 0; j < nodes; j++) {
+				if (r.nextDouble() < probability) {
+					graph.get(i).add(j);
+				}
+			}
+		}
 		System.out.println(graph);
 	}
 
@@ -30,7 +35,7 @@ public class Graph {
 		boolean done = false;
 		Random r = new Random();
 		while (!done) {
-			//System.out.println("Current State: " + state);
+			// System.out.println("Current State: " + state);
 			List<Integer> successors = graph.get(state);
 			if (successors.isEmpty()) {
 				done = true;
