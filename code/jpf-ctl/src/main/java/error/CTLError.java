4c667d15d688a3ae9dee1be08bf0469f3627cdaf
Anto Nanah Ji <antoji@my.yorku.ca>
2021-07-13 10:06:39 -0400

    Updated CTLError class

--- code/jpf-ctl/src/main/java/error/CTLError.java
+++ code/jpf-ctl/src/main/java/error/CTLError.java
@@ -2,6 +2,8 @@ package error;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 
@@ -20,6 +22,8 @@ public class CTLError {
 	static int inputLineNum = 0;
 	// To verify if the fields exists in the user input
 	boolean fieldNotExist = false;
+	
+	StringBuilder errMsg = new StringBuilder();
 
 	/**
 	 * Inserts the Java reserved words into the reservedWordsSet Hash set. It also
@@ -48,86 +52,130 @@ public class CTLError {
 	 * @return recovered input if there is an error, the input formal otherwise.
 	 */
 	public CharStream errorCheckAndRecover(CharStream input) {
-		inputLineNum++; // input line number
-		StringBuilder result = new StringBuilder(); // the recovered input
+
+		inputLineNum++;									//input line number
+		StringBuilder recovedInput = new StringBuilder();		//the recovered input
 		String inputString = input.toString();
 		String[] lines = inputString.split(" ");
 		int size = lines.length;
-		int index = 0; // the error character index in the input string
+		int index = 0;									//the error character index in the input string
 		boolean hasError = false;
-		// cWriter.printlnout("Initial input: " + inputString);
 
+		
 		for (int i = 0; i < size; i++) {
-
-			// check and recover if the input formula missing the operators '&' or '|'
+			
+			//check and recover if the input formula missing the operators '&' or '|'
 			if (operatorsSet.contains(lines[i])) {
 				hasError = true;
-				// call underLineError method to print the error message
-				underLineError(inputString, index, lines[i]);
+				//call underLineError method to print the error message
+				underLineError(inputString, index, " token recognition error at: '"+ lines[i] + " '" );
 
-				result.append(lines[i]);
+				recovedInput.append(lines[i]);	
 			}
-			// check and recover if the input formula contains any Java reserve word
+			//check and recover if the input formula contains any Java reserve word
 			if (lines[i].contains(".")) {
-
+				
+				//check if the fields in the input exist
+				//FieldExists(lines[i],inputString, index);
+				
 				String[] substrings = lines[i].split("[.]");
-				// FieldExists(lines[i], inputString, index);
-
+			
 				for (int j = 0; j < substrings.length; j++) {
 
 					if (reservedWordsSet.contains(substrings[j])) {
 						hasError = true;
-						// call underLineError method to print the error message
-						underLineError(inputString, index, substrings[j]);
+						//call underLineError method to print the error message 
+						underLineError(inputString, index," token recognition error at: '"+ substrings[j] + " '" );
 						substrings[j] = substrings[j].toUpperCase();
 					}
-					result.append(substrings[j]);
-					if (j < substrings.length - 1) {
-						result.append(".");
+					recovedInput.append(substrings[j]);
+					if (j < substrings.length - 1)
+					{
+						recovedInput.append(".");							
 					}
-					index += substrings[j].length() + 1;
+					index += substrings[j].length() + 1;					
 				}
-
-			} else {
-				result.append(lines[i]);
+				
+				
+			} else 
+			{				
+				recovedInput.append(lines[i]);	
 				index += lines[i].length() + 1;
+			}			
+			recovedInput.append(" ");			
+		}	
+		
+		//if there is field not found error then terminate
+		if(fieldNotExist )
+		{
+			System.err.print( errMsg.toString() );
+			return null;
+			
+		}
+		//if there is error print message on the console and return the recovered input
+		if(hasError)
+		{
+			System.err.print(errMsg.toString() );
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+	
 			}
-			result.append(" ");
+			System.out.print("Initial   input: " + inputString + "\n" + "Recovered input: " + recovedInput.toString() + "\n");
+			
+			return CharStreams.fromString(recovedInput.toString());
 		}
-
-		// if there is field not found error then terminate
-		if (fieldNotExist) {
-			System.exit(1);
-		}
-
-		// if there is error return the recovered input
-		if (hasError) {
-			System.out.println("Recovered input: " + result.toString());
-			return CharStreams.fromString(result.toString());
-		}
-
-		return input;
+		
+		return input;		
 	}
-
+	
 	/**
-	 * This method prints the error messages on the console. It also underlines the
-	 * error location in the input.
+	 * This method verifies if the input fields exists 
+	 * If not, it will print the error messages on the console
 	 * 
-	 * @param errorLine          - input formula that contains error.
-	 * @param charPositionInLine - error location in the input.
-	 * @param errorChar          - reserved word or operator used in the input.
+	 * @param atomicProposition - atomic proposition to verify if its fields exist 
+	 * @param inputString - input formula.
+	 * @param errCharIndex - error location in the input. 
+	 * 
 	 */
-	private void underLineError(String errorLine, int charPositionInLine, String token) {
-		System.err.println("line " + inputLineNum + ":" + (charPositionInLine + 1) + " token recognition error at: '"
-				+ token + "'");
-		System.err.println(errorLine);
-		// To underlines the error location
-		for (int i = 0; i < charPositionInLine; i++)
-			System.err.print(" ");
-
-		for (int i = 0; i < token.length(); i++)
-			System.err.print("^");
-
-		System.err.println();
+	private void FieldExists(String atomicProposition, String inputString, int errCharIndex )
+	{
+		int indexOfLastDot = atomicProposition.lastIndexOf(".");
+        String className = atomicProposition.substring(0, indexOfLastDot);
+        String fieldName = atomicProposition.substring(indexOfLastDot + 1);
+        
+		try {
+           Class.forName(className).getDeclaredField(fieldName);
+ 
+        } catch (ClassNotFoundException e) { 
+        	fieldNotExist = true;
+        	underLineError(inputString, errCharIndex," Class '"+ className + " ' cannot be found" );
+        	
+        } catch (NoSuchFieldException | SecurityException e) {     
+        	fieldNotExist = true;
+        	underLineError(inputString, errCharIndex + indexOfLastDot + 1," Field '"+ fieldName + " ' cannot be found" );
+        	
+        }
+	}
+	
+	/**
+	 * This method prints the error messages on the console.
+	 * It also underlines the error location in the input.
+	 * 
+	 * @param errorLine	- input formula that contains error.
+	 * @param charPositionInLine - error location in the input.
+	 * @param errorMsg - error message.
+	 */
+	private void underLineError(String errorLine, int charPositionInLine, String errorMsg )
+	{
+		
+		errMsg.append("\nline "+ inputLineNum +":"+ (charPositionInLine + 1) + errorMsg + "\n");
+		errMsg.append(errorLine+ "\n");
+		//To underlines the error location
+		for(int i=0; i<charPositionInLine; i++)
+			errMsg.append(" ");
+		
+		errMsg.append("^"+ "\n");
 	}
 }
+
