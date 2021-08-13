2a65cec93338fa76562e6ef6b094822bdbffb953
ParssaKhazra <parssa.khazra@gmail.com>
2021-08-13 16:56:05 -0400

    added output window and save button

--- code/jpf-ctl/src/main/java/gui/Interface.java
+++ code/jpf-ctl/src/main/java/gui/Interface.java
@@ -8,7 +8,10 @@ import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -20,7 +23,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileFilter;
@@ -154,6 +159,12 @@ public class Interface extends JFrame {
 		btn_run.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+			
+				//testing
+				outputPane( "Model Checking Finished\n For the selected class:\t" + "c:users/desktop/example.class"
+						+ "\n And the written formula:\t" + "AX true"
+						+ "\nIt has been determined that the formula holds in the initial state and is considered valid for this system.", "");
+				
 				JFrame frame = new JFrame();
 				if (ta_formula.getText().trim().isEmpty()) {
 					// joption pane for error
@@ -177,6 +188,7 @@ public class Interface extends JFrame {
 									+ "\n And the written formula:\t" + formula
 									+ "\nIt has been determined that the formula holds in the initial state and is considered valid for this system.";
 							JOptionPane.showMessageDialog(frame, msg);
+							
 						} else {
 							String msg = "Model Checking Finished\n For the selected class:\t" + path
 									+ "\n And the written formula:\t" + formula
@@ -214,6 +226,86 @@ public class Interface extends JFrame {
 		// add(contentPane);
 	}
 
+	public void outputPane(String msg, String counterExample) 
+	{
+		JFrame outputWindow = new JFrame();
+		outputWindow.setSize(800, 360);
+		outputWindow.setResizable(false);
+		outputWindow.setTitle("jpf-ctl-output");
+		outputWindow.setLocationRelativeTo(null);
+		
+		outputWindow.setLayout(new BoxLayout(outputWindow.getContentPane(), BoxLayout.Y_AXIS)); 
+		
+		JTextArea ta_output = new JTextArea();
+		ta_output.setText(msg);
+	    //ta_output.append("\n-----------------------");
+		ta_output.setEditable(false);
+		ta_output.setLineWrap(true);
+		ta_output.setPreferredSize(new Dimension(750,250));
+		
+		JScrollPane scroll = new JScrollPane(ta_output);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		JButton btn_save = new JButton("Save");
+		btn_save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			
+				JFileChooser fc = new JFileChooser();
+				fc.setFileFilter(new FileFilter() {
+					@Override
+					public String getDescription() {
+						return "Text Files (*.txt)";
+					}
+
+					@Override
+					public boolean accept(File f) {
+						if (f.isDirectory())
+							return true;
+						else
+							return f.getName().toLowerCase().endsWith(".class");
+					}
+				});		
+				
+				int returnval = fc.showSaveDialog(btn_save);
+				
+				if(returnval == JFileChooser.APPROVE_OPTION)
+				{
+					File f = fc.getSelectedFile();
+					try 
+					{
+						BufferedWriter out = new BufferedWriter(new FileWriter(f));
+						out.write(ta_output.getText()); // put the final string
+						out.close();
+						
+						JOptionPane.showMessageDialog(null, "File Saved", "Saved",
+								JOptionPane.INFORMATION_MESSAGE);
+						
+					} 
+					catch (IOException e1) {e1.printStackTrace();}
+				}		
+			}	
+		});
+		
+		
+		JPanel savePane = new JPanel();
+		savePane.setLayout(new GridLayout(1,4,10,10));
+		savePane.setPreferredSize(new Dimension(750, 50));
+		savePane.add(new JLabel());
+		savePane.add(new JLabel());
+		savePane.add(new JLabel());
+		savePane.add(btn_save);
+		
+		JPanel contentPane = new JPanel();
+		contentPane.add(scroll);
+		contentPane.add(savePane);
+			
+		outputWindow.setContentPane(contentPane);
+		outputWindow.setVisible(true);
+		
+	}
+	
 	public static void main(String[] args) {
 		new Interface();
 	}
