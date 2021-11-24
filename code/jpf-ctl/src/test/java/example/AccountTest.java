55ba7fed8eca433403a96bae3cb9b1a2d1ef02a7
Franck van Breugel <franck@eecs.yorku.ca>
2021-11-23 19:12:08 -0500

    refactor

--- code/jpf-ctl/src/test/java/example/AccountTest.java
+++ code/jpf-ctl/src/test/java/example/AccountTest.java
@@ -2,29 +2,27 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 class AccountTest {
 
-	@Test
+	private static final int TRIALS = 1000;
+
+	@RepeatedTest(TRIALS)
 	void test() {
-		final int TRIALS = 1000000;
-		
 		final int INITIAL = 1;
 		final int DEPOSIT = 0;
 		final int WITHDRAW = 2;
 		
-		for (int t = 0; t < TRIALS; t++) {
-			Account account = new Account();
-			account.deposit(INITIAL);
-			for (int d = 0; d < DEPOSIT; d++) {
-				new Deposit("d" + d, account).start();
-				assertTrue(account.getBalance() >= 0);
-			}
-			for (int w = 0; w < WITHDRAW; w++) {
-				new Withdraw("w" + w, account).start();
-				assertTrue(account.getBalance() >= 0);
-			}
+		Account account = new Account();
+		account.deposit(INITIAL);
+		for (int d = 0; d < DEPOSIT; d++) {
+			new Deposit("d" + d, account).start();
+			assertTrue(account.getBalance() >= 0);
+		}
+		for (int w = 0; w < WITHDRAW; w++) {
+			new Withdraw("w" + w, account).start();
+			assertTrue(account.getBalance() >= 0);
 		}
 	}
 }
