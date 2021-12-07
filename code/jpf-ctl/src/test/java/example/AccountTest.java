3f093ebb5850c00a2129e2889c690f183cfaea51
matthewwalk <matt.walker98@outlook.com>
2021-12-07 10:44:31 -0500

    Revert "Delete AccountTest.java"
    This reverts commit 3fe2d542951e1e4a22fbaf8814914b851cb25bc2.

---
+++ code/jpf-ctl/src/test/java/example/AccountTest.java
@@ -0,0 +1,28 @@
+package example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+class AccountTest {
+
+	private static final int TRIALS = 1000;
+
+	@RepeatedTest(TRIALS)
+	void test() {
+		final int INITIAL = 1;
+		final int DEPOSIT = 0;
+		final int WITHDRAW = 2;
+		
+		Account account = new Account();
+		account.deposit(INITIAL);
+		for (int d = 0; d < DEPOSIT; d++) {
+			new Deposit("d" + d, account).start();
+			assertTrue(account.getBalance() >= 0);
+		}
+		for (int w = 0; w < WITHDRAW; w++) {
+			new Withdraw("w" + w, account).start();
+			assertTrue(account.getBalance() >= 0);
+		}
+	}
+}
